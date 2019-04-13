0d69c0632cfd6e0edb7212088261bbbd1d7af9c0
venkatesh <venkatesh@venkatesh.local>
2019-04-13 20:45:45 +0530

    seeder to create user

--- src/v1/helpers/db.js
+++ src/v1/helpers/db.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-console.log('inside db', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
