efe49bf835d8157a3c4596be59020fb2e7c4a646
venkatesh <venkatesh@venkatesh.local>
2019-04-13 19:04:49 +0530

    login handler added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app        = express();
 const cors = require('cors');
 var bodyParser = require('body-parser');
 require('dotenv').config();
+const sessionsHandler = require('./src/v1/handlers/session.handlers');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,6 +20,8 @@ app.use('/api', router);
 router.get('/', (req, res) => {
   res.json({data: "Welcome"});
 });
+
+router.post('/signin', sessionsHandler.signin);
 
 app.listen(port);
 console.log('Server started at: localhost:' + port);
--- src/v1/handlers/session.handlers.js
+++ src/v1/handlers/session.handlers.js
@@ -5,15 +5,15 @@ const User = db.User;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const signin = async(req, res) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    const user = await User.findOne({username});
+  const { email, password } = req.body;
+  if (email && password) {
+    const user = await User.findOne({email});
     if (user && bcrypt.compareSync(password, user.password)) {
-          const token = jwt.sign({ username: user.username }, JWT_SECRET);
-      return res.json({message: 'Login Successfull!!', success: true, token, username: user.username });
+          const token = jwt.sign({ email: user.email }, JWT_SECRET);
+      return res.json({message: 'Login Successfull!!', success: true, token, email: user.email });
     }
   }
-  return res.status(422).json({ message: 'username/password is invalid', success: false });
+  return res.status(422).json({ error: 'username/password is invalid', success: false });
 };
 
 module.exports.signin = signin;
